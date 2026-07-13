e14f3a1a643b67711a240f32bcb44e52d6e55b89
sergio <sergiohg818@gmail.com>
2026-07-13 22:14:55 +0200

    package: Add rtsyn-port

---
+++ packages/r/rtsyn-port/xmake.lua
@@ -0,0 +1,19 @@
+includes(path.join(os.scriptdir(), "..", "..", "..", "includes", "rtsyn_source.lua"))
+includes(path.join(os.scriptdir(), "..", "..", "..", "includes", "rtsyn_install.lua"))
+
+local package_name = "rtsyn-port"
+
+package(package_name)
+
+set_homepage("https://github.com/seregioo/" .. package_name)
+set_description("C library for creating a port for RTSyn")
+set_license("GPL-3.0-or-later")
+
+add_deps("rtsyn-abi", "rtsyn-value")
+rtsyn_source(package_name, "https://github.com/seregioo/" .. package_name .. ".git")
+
+rtsyn_install(package_name)
+
+on_test(function(package)
+	assert(package:has_cincludes("rtsyn/port.h"))
+end)
