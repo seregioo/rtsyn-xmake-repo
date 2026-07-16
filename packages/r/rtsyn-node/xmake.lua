acd384fb8be9738fce3d7343619f66704633d361
sergio <sergiohg818@gmail.com>
2026-07-16 18:33:05 +0200

    package: Add rtsyn-node

---
+++ packages/r/rtsyn-node/xmake.lua
@@ -0,0 +1,19 @@
+includes(path.join(os.scriptdir(), "..", "..", "..", "includes", "rtsyn_source.lua"))
+includes(path.join(os.scriptdir(), "..", "..", "..", "includes", "rtsyn_install.lua"))
+
+local package_name = "rtsyn-node"
+
+package(package_name)
+
+set_homepage("https://github.com/seregioo/" .. package_name)
+set_description("C library for creating a node for RTSyn")
+set_license("GPL-3.0-or-later")
+
+add_deps("rtsyn-abi", "rtsyn-value", "rtsyn-port", "rtsyn-collection")
+rtsyn_source(package_name, "https://github.com/seregioo/" .. package_name .. ".git")
+
+rtsyn_install(package_name)
+
+on_test(function(package)
+	assert(package:has_cincludes("rtsyn/node.h"))
+end)
