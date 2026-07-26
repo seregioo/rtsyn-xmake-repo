6d9e37dc378dc04ee2b360a587405ee82de047c9
seregio <sergiohg818@gmail.com>
2026-07-26 12:54:36 +0200

    package: Add rtsyn-collection

---
+++ packages/r/rtsyn-collection/xmake.lua
@@ -0,0 +1,18 @@
+includes(path.join(os.scriptdir(), "..", "..", "..", "includes", "rtsyn_source.lua"))
+includes(path.join(os.scriptdir(), "..", "..", "..", "includes", "rtsyn_install.lua"))
+
+local package_name = "rtsyn-collection"
+
+package(package_name)
+
+set_homepage("https://github.com/seregioo/" .. package_name)
+set_description("C library for creating a collection for RTSyn")
+set_license("GPL-3.0-or-later")
+
+rtsyn_source(package_name, "https://github.com/seregioo/" .. package_name .. ".git")
+
+rtsyn_install(package_name)
+
+on_test(function(package)
+	assert(package:has_cincludes("rtsyn/collection.h"))
+end)
